c075358b10ead5e1e53e602c21ad52caf8484303
YanCamiloDev <yancamilo4@gmail.com>
2022-07-23 13:24:35 -0300

    Preparando requisição para buscar os pokemons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,28 @@ import SearchComponent from '../components/SearchComponent';
 import Icone from '../assets/icone.png';
 import { Button, ContainerFlex, Corpo, DefaultLayout, Text } from '../StyledComponents';
 import PokeCard from '../components/PokeCard';
+import api from '../services';
+import { AllDataPokemons, Pokemon } from '../types';
+import axios from 'axios';
 
 export default function Home() {
+  const [pokemons, setPokemons] = React.useState<Pokemon[]>([]);
+
+  React.useEffect(() => {
+    async function getPokemons() {
+      const list = await api.get(`/pokemon/?limit=20&offset=20`);
+      const data = list.data as AllDataPokemons;
+      for (const result of data.results) {
+        const dados = await axios.get(result.url);
+        setPokemons((old) => [...old, dados.data])
+      }
+    }
+    getPokemons()
+  }, [])
+
+  React.useEffect(() => {
+    console.log(pokemons);
+  }, [pokemons])
   return (
     <DefaultLayout>
       <Header />
