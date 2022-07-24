1573c64992a35abd2ab4a6c7007fd73602247f35
YanCamiloDev <yancamilo4@gmail.com>
2022-07-24 17:22:28 -0300

    Criando readme

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/impar-frontapp/',
-  envPrefix: ['BASE_URL'],
+  server: {
+    port: 3001
+  },
   plugins: [react()]
 })
