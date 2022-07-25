2a70eb44dff7ff89a43387b521ae734e3c9fd0bb
yancamilo <yancamilo8@gmail.com>
2022-07-25 16:14:58 -0300

    Ajuste final

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # Impar-frontapp Yan
-> App criado por Yan Souza Camilo. 
 
-> Disponível em: https://yancamilodev.github.io/impar-frontapp/
+> App criado por Yan Souza Camilo.
+
+<!-- > Disponível em: https://yancamilodev.github.io/impar-frontapp/ -->
+
 #
+
 ## INSTRUÇÕES PARA EXECUTAR O PROJETO EM SUA MÁQUINA:
 
- - Baixe o projeto localmente;
- - Abra um terminal na pasta do projeto e rode 'yarn' ou 'npm install' (Sem aspas);
- - Por fim, execute 'yarn dev' (Sem aspas) para iniciar o projeto;
+- Baixe o projeto localmente;
+- Abra um terminal na pasta do projeto e rode 'yarn' ou 'npm install' (Sem aspas);
+- Por fim, execute 'yarn dev' (Sem aspas) para iniciar o projeto;
 
 ## VISUALIZAÇÃO DO PROJETO:
-- OPÇÃO 01: Acesse o site publicado no endereço: https://yancamilodev.github.io/impar-frontapp/
-- OPÇÃO 02: Realize os passos de execução e posteriormente abra um navegador em http://localhost:3001/impar-frontapp/
 
+<!-- - OPÇÃO 01: Acesse o site publicado no endereço: https://yancamilodev.github.io/impar-frontapp/ -->
 
+- Realize os passos de execução e posteriormente abra um navegador em http://localhost:3001/impar-frontapp/
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import SearchComponent from '../components/SearchComponent';
-import { Button, ContainerFlex, Corpo, DefaultLayout, PaginationComp, Skeleton, Text, TextH1 } from '../StyledComponents';
+import {
+  Button,
+  ContainerFlex,
+  Corpo,
+  DefaultLayout,
+  PaginationComp,
+  Skeleton,
+  Text,
+  TextH1,
+} from '../StyledComponents';
 import PokeCard from '../components/PokeCard';
 import api from '../services';
 import { AllDataPokemons, Pokemon } from '../types';
@@ -9,7 +18,7 @@ import axios from 'axios';
 import { Pagination } from '@mui/material';
 import AlertInfo from '../components/AlertInfo';
 import { useParams } from 'react-router-dom';
-const ROWS_PER_PAGE = 15
+const ROWS_PER_PAGE = 15;
 
 export default function Home() {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
@@ -20,40 +29,44 @@ export default function Home() {
   let { pokemonPesquisa } = useParams();
   async function getAllPokemons() {
     try {
-      setLoading(true)
-      const list = await api.get(`/pokemon/?limit=${ROWS_PER_PAGE}&offset=${page * ROWS_PER_PAGE}`);
+      setLoading(true);
+      const list = await api.get(
+        `/pokemon/?limit=${ROWS_PER_PAGE}&offset=${page * ROWS_PER_PAGE}`
+      );
       const data = list.data as AllDataPokemons;
       setCount(data.count);
       let array = [];
       for (const result of data.results) {
         const dados = await axios.get(result.url);
-        array.push(dados.data)
+        array.push(dados.data);
       }
-      setPokemons(array)
-      setLoading(false)
+      setPokemons(array);
+      setLoading(false);
     } catch (error) {
-      setLoading(false)
+      setLoading(false);
     }
   }
 
   async function searchPokemon() {
     try {
-      setLoading(true)
-      const list = await api.get(`/pokemon/${pokemonPesquisa?.trim().toLowerCase()}`);
-      setPokemons([list.data])
-      setLoading(false)
+      setLoading(true);
+      const list = await api.get(
+        `/pokemon/${pokemonPesquisa?.trim().toLowerCase()}`
+      );
+      setPokemons([list.data]);
+      setLoading(false);
     } catch (error) {
-      setLoading(false)
-      setPokemons([])
+      setLoading(false);
+      setPokemons([]);
     }
   }
   useEffect(() => {
-    pokemonPesquisa ? searchPokemon() : getAllPokemons()
-  }, [page, pokemonPesquisa])
+    pokemonPesquisa ? searchPokemon() : getAllPokemons();
+  }, [page, pokemonPesquisa]);
 
   function handleChangePage(event: React.ChangeEvent<unknown>, value: number) {
-    setPage(value)
-    document.querySelector('body')?.scrollTo(0, 260)
+    setPage(value);
+    window.scrollTo(0, 260);
   }
 
   return (
@@ -61,39 +74,65 @@ export default function Home() {
       <Header />
       <Corpo>
         <SearchComponent />
-        <main style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-          <AlertInfo
-            infoAlert={infoAlert}
-            setInfoAlert={setInfoAlert} />
-          <ContainerFlex width='70%' fd='column'>
-            <ContainerFlex width='100%' fw='wrap' fd='row' ai="center" jc='space-between'>
-              <Text as={TextH1} size='32px' weight={500} >
-                {pokemonPesquisa ? 'Resultado da pesquisa' : 'Lista de Pokémons'}
+        <main
+          style={{ width: '100%', display: 'flex', justifyContent: 'center' }}
+        >
+          <AlertInfo infoAlert={infoAlert} setInfoAlert={setInfoAlert} />
+          <ContainerFlex width="70%" fd="column">
+            <ContainerFlex
+              width="100%"
+              fw="wrap"
+              fd="row"
+              ai="center"
+              jc="space-between"
+            >
+              <Text as={TextH1} size="32px" weight={500}>
+                {pokemonPesquisa
+                  ? 'Resultado da pesquisa'
+                  : 'Lista de Pokémons'}
               </Text>
-              {loading ? <Skeleton w='183px' h='60px' /> : <Button onClick={() => setInfoAlert('Função não implementada')}>Novo Pokémon</Button>}
+              {loading ? (
+                <Skeleton w="183px" h="60px" />
+              ) : (
+                <Button onClick={() => setInfoAlert('Função não implementada')}>
+                  Novo Pokémon
+                </Button>
+              )}
             </ContainerFlex>
-            <ContainerFlex centerOnMobile={true} fw='wrap' mt='30px' width='100%' fd='row'>
-              {loading && [...Array(10).keys()].map((item, index) => {
-                return (
-                  <Skeleton key={`sk-${index}`} />
-                )
-              })}
+            <ContainerFlex
+              centerOnMobile={true}
+              fw="wrap"
+              mt="30px"
+              width="100%"
+              fd="row"
+            >
+              {loading &&
+                [...Array(ROWS_PER_PAGE).keys()].map((item, index) => {
+                  return <Skeleton key={`sk-${index}`} />;
+                })}
 
               {!loading && pokemons.length === 0 && (
-                <Text size='20px' mb='30%' weight={500} mt="20px" color="#263238" >
+                <Text
+                  size="20px"
+                  mb="30%"
+                  weight={500}
+                  mt="20px"
+                  color="#263238"
+                >
                   Nenhum pokémon encontrado :(
                 </Text>
               )}
-              {pokemons.map((item, index) => {
-                return (
-                  <PokeCard
-                    key={`pokemon-${index}`}
-                    image={item.sprites.front_default}
-                    desc={item.name}
-                    alertInfo={setInfoAlert}
-                  />
-                )
-              })}
+              {!loading &&
+                pokemons.map((item, index) => {
+                  return (
+                    <PokeCard
+                      key={`pokemon-${index}`}
+                      image={item.sprites.front_default}
+                      desc={item.name}
+                      alertInfo={setInfoAlert}
+                    />
+                  );
+                })}
             </ContainerFlex>
             {!pokemonPesquisa && (
               <PaginationComp>
@@ -105,10 +144,9 @@ export default function Home() {
                 />
               </PaginationComp>
             )}
-
           </ContainerFlex>
         </main>
       </Corpo>
-    </DefaultLayout >
-  )
+    </DefaultLayout>
+  );
 }
